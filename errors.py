d7679e67ce26cb0c5eeed13e6274092dc2aebd2d
Craig Bourne <craigbourne15@gmail.com>
2024-12-16 09:14:11 +0000

    init commit

---
+++ errors.py
@@ -0,0 +1,21 @@
+'''
+Activity 1: Errors
+Incorporate the following code into a Python program to handle exceptions.
+'''
+
+try:
+  # do something
+  pass
+
+except ValueError:
+  # handle ValueError exception
+  pass
+
+except (TypeError, ZeroDivisionError):
+  # handle multiple exceptions
+  # TypeError and ZeroDivisionError
+  pass
+
+except:
+  # handle all other exceptions
+  pass
